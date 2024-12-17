9392785e67cc824b1dae122ad282373a16a076a0
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-12-17 23:18:15 +0530

    Update Schema.js

--- Schema.js
+++ Schema.js
@@ -4,11 +4,20 @@ const userSchema = new mongoose.Schema({
 
     username: String,
     password: String,
-    todos:[]
+     todos:[]
 
 
 })
 
+
+// const todoSchema = new mongoose.Schema({
+
+//     username:String,
+//     data:String
+    
+// })
+
+//const todoModel = mongoose.model("todoModel",todoSchema)
 const userModel = mongoose.model("userModel",userSchema)
 
-module.exports = {userModel}
+module.exports = {userModel,//todoModel}
